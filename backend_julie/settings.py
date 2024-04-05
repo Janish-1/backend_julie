a85d1386568c3cf5fba0c1a600821ca52b027580
Janish-1 <janish.pancholi11@gmail.com>
2024-04-05 16:15:58 +0530

    update

--- backend_julie/settings.py
+++ backend_julie/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     # 2 Added App
     'imager',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
