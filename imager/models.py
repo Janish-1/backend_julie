b247b930b9074a627d83ab26b7f4e5679b263df2
Janish-1 <janish.pancholi11@gmail.com>
2024-04-05 15:04:21 +0530

    fixed bug

--- imager/models.py
+++ imager/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Image(models.Model):
     name = models.CharField(max_length=255)
-    url = models.ImageField('../media/')
+    url = models.ImageField('../media/')  # Relative path from MEDIA_ROOT
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self) -> str:
-        return self.name
+    def __str__(self):
+        return self.name + self.url
